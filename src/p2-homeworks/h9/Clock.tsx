eeb8a8ca9142e0168c3642c38fdedb6928fa8ae1
fanur56 <ilasovf@gmail.com>
2022-08-14 15:58:07 +0300

    HW9 solved

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,31 +1,33 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from "./Clock.module.css"
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
+        console.log(date.getHours())
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date.toLocaleTimeString("ru-RU") // fix with date
+    const stringDate = date.toLocaleDateString("ru-RU")// fix with date
 
     return (
         <div>
@@ -33,15 +35,15 @@ function Clock() {
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
-                {stringTime}
+                {(date.getHours() < 10) ? "0" + stringTime : stringTime}
             </div>
-
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
-
+            <div className={s.date}>
+                {show && (
+                    <div>
+                        {stringDate}
+                    </div>
+                )}
+            </div>
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
 
